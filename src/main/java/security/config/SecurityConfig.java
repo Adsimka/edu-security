a05cbf1bdb0401697d50675394fa032f930ae276
Arsen <aminnegulov@7tech.ru>
2025-09-15 21:42:03 +0300

    Issue 4. Configure entry point

--- src/main/java/security/config/SecurityConfig.java
+++ src/main/java/security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @Configuration
 public class SecurityConfig {
@@ -14,13 +15,17 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/public/login.html").permitAll()
+                        .requestMatchers("/public/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
                         .loginPage("/public/login.html").permitAll()
                         .loginProcessingUrl("/login") // URL для обработки username и password (по умл - своей реализации нет)
                         .defaultSuccessUrl("/api/v1/welcome") // ссылка при успешной аутентификации
+                )
+                .exceptionHandling(exception ->
+                        // При попытке зайти на любую страницу, которая защищена аутентификацией, мы будем перенаправлены на /public/error.html
+                        exception.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/public/error.html"))
                 )
                 .build();
     }
