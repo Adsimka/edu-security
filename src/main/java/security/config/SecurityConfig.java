66f49b861b54a74502653a3f5cc6715161d51388
Arsen <aminnegulov@7tech.ru>
2025-09-22 21:47:41 +0300

    Issue 9. Create custom filter

--- src/main/java/security/config/SecurityConfig.java
+++ src/main/java/security/config/SecurityConfig.java
@@ -3,11 +3,12 @@ package security.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.session.DisableEncodeUrlFilter;
+import security.config.filter.DeniedClientFilter;
 import security.service.JdbcUserDetailsService;
 
 import javax.sql.DataSource;
@@ -17,27 +18,16 @@ import javax.sql.DataSource;
 public class SecurityConfig {
 
     /**
-     *  Custom configurer
+     *  Custom filter
      */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        log.info("AuthenticationManager in config: {}", http.getSharedObject(AuthenticationManager.class));
-        http.apply(new CustomSecurityConfigurer())
-                .realmName("Custom realm name");
-        return http.build();
-
+        return http
+                .addFilterBefore(new DeniedClientFilter(), DisableEncodeUrlFilter.class)
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+                .build();
     }
-
-    /**
-     *  Basic authenticated
-     */
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .httpBasic(Customizer.withDefaults())
-//                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
-//                .build();
-//    }
 
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
